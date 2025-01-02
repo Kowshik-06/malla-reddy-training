3f522bd3b7b5af14d9323f66b04a8e8c669a5ea1
Kowshik-06 <151979552+Kowshik-06@users.noreply.github.com>
2025-01-02 12:05:58 +0530

    greatest of three numbers

--- find_greater.java
+++ find_greater.java
@@ -1,12 +1,9 @@
 public class find_greater {
     public static void main (String[] args){
-        int num1 = 10, num2 = 25, num3 = 15;
+        int num1 = 10, num2 = 25;
         int greatest = num1;
         if (num2 > greatest){
             greatest = num2; 
-        }
-        if (num 3 > greatest){
-            greatest = num3;
         }
         System.out.println("The greatest number is: " + greatest);
 
