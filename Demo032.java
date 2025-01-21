82bc635e23741d4c6c2a07b3b7790cfd87e89ce8
Kowshik-06 <151979552+Kowshik-06@users.noreply.github.com>
2025-01-21 10:11:00 +0530

    Add files via upload

---
+++ Demo032.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class Demo032 {
+    public static void main(String[] args) {
+        HashSet<String> stringSet = new HashSet<String>();
+        stringSet.add("Varshith");
+        stringSet.add("Srujan");
+        stringSet.add("Abhi");
+        stringSet.add("Lokesh");
+        System.out.println("Elements in the HashSet: " + stringSet);
+    }
+}
