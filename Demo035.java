38c907857d6996d3c44bff5fb4f59a66a29d633a
Kowshik-06 <151979552+Kowshik-06@users.noreply.github.com>
2025-01-25 10:31:37 +0530

    Add files via upload

---
+++ Demo035.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Demo035{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int num1 =sc.nextInt();
+        System.out.print("Enter the second number:");
+        int num2 =sc.nextInt();
+
+        if(num1<num2){
+            System.out.println(num2+" is greater than the "+num1);
+        }
+        else if(num1>num2){
+            System.out.println(num1+" is greater than the "+num2);
+        }
+        else{
+            System.out.println("Given number are equal");
+        }
+    }
+}
